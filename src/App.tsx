6fb23b4e060a18d82f963e47a57133bfd21594cf
Amine Kamal <amine.kamal@sgdigital.com>
2022-02-01 08:14:46 -0500

    add resume template and split in components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import About from "./components/About";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import PhotoSwipe from "./components/PhotoSwipe";
+import Portfolio from "./components/Portfolio";
+import Preloader from "./components/Preloader";
+import Services from "./components/Services";
+import Testimonials from "./components/Testimonials";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Preloader/>
+      <Header/>
+      <Hero/>
+      <About/>
+      <Services/>
+      <Portfolio/>
+      <Testimonials/>
+      <Footer/>
+      <PhotoSwipe/>
+    </>
   );
 }
 
---
+++ src/components/Preloader.tsx
@@ -0,0 +1,13 @@
+
+export default function Preloader() 
+{
+    return (
+        <div id="preloader">
+            <div id="loader" className="dots-fade">
+                <div />
+                <div />
+                <div />
+            </div>
+        </div>
+    );
+} 
